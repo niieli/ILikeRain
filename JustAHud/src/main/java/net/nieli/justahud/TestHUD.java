118ea293b2d4cb1c83622f25226983826eeb1d8c
niieli <96092663+niieli@users.noreply.github.com>
2022-01-17 09:47:37 +0100

    Test HUD

--- JustAHud/src/main/java/net/nieli/justahud/TestHUD.java
+++ JustAHud/src/main/java/net/nieli/justahud/TestHUD.java
@@ -1,15 +1,19 @@
 package net.nieli.justahud;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class TestHUD extends DrawableHelper implements HudRenderCallback {
+    private static final Identifier TEST = new Identifier(JustAHud.MODID, "textures/hud/ardispolicestop.png");
     @Override
     public void onHudRender(MatrixStack matrixStack, float tickDelta) {
-        DrawableHelper.fill(matrixStack,20,20,200,200, (int) 2201441830.64);
+        RenderSystem.setShaderTexture(0, TEST);
+        DrawableHelper.drawTexture(matrixStack,200,20,0,0,0,100,100,100,100);
     }
 }
